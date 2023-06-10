c8eef1728566b619217b5d21c4aa3a82dcb98426
bilalcaliskan <bilalcaliskan@protonmail.com>
2023-06-10 12:42:26 +0300

    test(bucketpolicy): increase test coverage

--- cmd/bucketpolicy/add/add_test.go
+++ cmd/bucketpolicy/add/add_test.go
@@ -2,7 +2,10 @@ package add
 
 import (
 	"context"
+	"errors"
 	"testing"
+
+	"github.com/aws/aws-sdk-go/service/s3/s3iface"
 
 	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/bilalcaliskan/s3-manager/cmd/root/options"
@@ -10,25 +13,27 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-/*var (
-	defaultGetBucketPolicyOutput = &s3.GetBucketPolicyOutput{
-		Policy: aws.String("{}"),
-	}
-	defaultGetBucketPolicyErr error
-)*/
+var (
+	defaultPutBucketPolicyOutput = &s3.PutBucketPolicyOutput{}
+	defaultPutBucketPolicyErr    error
+)
 
 func createSvc(rootOpts *options.RootOptions) (*s3.S3, error) {
 	return internalaws.CreateAwsService(rootOpts)
 }
 
 // Define a mock struct to be used in your unit tests
-/*type mockS3Client struct {
+type mockS3Client struct {
 	s3iface.S3API
-}*/
+}
 
 /*func (m *mockS3Client) GetBucketPolicy(input *s3.GetBucketPolicyInput) (*s3.GetBucketPolicyOutput, error) {
 	return defaultGetBucketPolicyOutput, defaultGetBucketPolicyErr
 }*/
+
+func (m *mockS3Client) PutBucketPolicy(input *s3.PutBucketPolicyInput) (*s3.PutBucketPolicyOutput, error) {
+	return defaultPutBucketPolicyOutput, defaultPutBucketPolicyErr
+}
 
 func TestExecuteTooManyArguments(t *testing.T) {
 	rootOpts := options.GetRootOptions()
@@ -80,7 +85,7 @@ func TestExecuteNoArgument(t *testing.T) {
 	bucketPolicyOpts.SetZeroValues()
 }
 
-/*func TestExecuteSuccessEnabled(t *testing.T) {
+func TestExecuteFailureFileNotFound(t *testing.T) {
 	rootOpts := options.GetRootOptions()
 	rootOpts.AccessKey = "thisisaccesskey"
 	rootOpts.SecretKey = "thisissecretkey"
@@ -93,18 +98,68 @@ func TestExecuteNoArgument(t *testing.T) {
 	mockSvc := &mockS3Client{}
 	svc = mockSvc
 
-	defaultGetBucketPolicyErr = nil
-
 	AddCmd.SetContext(context.WithValue(AddCmd.Context(), options.S3SvcKey{}, svc))
 	AddCmd.SetContext(context.WithValue(AddCmd.Context(), options.OptsKey{}, rootOpts))
 
 	AddCmd.SetArgs([]string{"dummy.json"})
 	err := AddCmd.Execute()
+	assert.NotNil(t, err)
+
+	rootOpts.SetZeroValues()
+	bucketPolicyOpts.SetZeroValues()
+}
+
+func TestExecuteSuccess(t *testing.T) {
+	rootOpts := options.GetRootOptions()
+	rootOpts.AccessKey = "thisisaccesskey"
+	rootOpts.SecretKey = "thisissecretkey"
+	rootOpts.Region = "thisisregion"
+	rootOpts.BucketName = "thisisbucketname"
+
+	ctx := context.Background()
+	AddCmd.SetContext(ctx)
+
+	mockSvc := &mockS3Client{}
+	svc = mockSvc
+
+	defaultPutBucketPolicyErr = nil
+
+	AddCmd.SetContext(context.WithValue(AddCmd.Context(), options.S3SvcKey{}, svc))
+	AddCmd.SetContext(context.WithValue(AddCmd.Context(), options.OptsKey{}, rootOpts))
+
+	AddCmd.SetArgs([]string{"../../../mock/bucketpolicy.json"})
+	err := AddCmd.Execute()
 	assert.Nil(t, err)
 
 	rootOpts.SetZeroValues()
 	bucketPolicyOpts.SetZeroValues()
-}*/
+}
+
+func TestExecutePutError(t *testing.T) {
+	rootOpts := options.GetRootOptions()
+	rootOpts.AccessKey = "thisisaccesskey"
+	rootOpts.SecretKey = "thisissecretkey"
+	rootOpts.Region = "thisisregion"
+	rootOpts.BucketName = "thisisbucketname"
+
+	ctx := context.Background()
+	AddCmd.SetContext(ctx)
+
+	mockSvc := &mockS3Client{}
+	svc = mockSvc
+
+	defaultPutBucketPolicyErr = errors.New("dummy error")
+
+	AddCmd.SetContext(context.WithValue(AddCmd.Context(), options.S3SvcKey{}, svc))
+	AddCmd.SetContext(context.WithValue(AddCmd.Context(), options.OptsKey{}, rootOpts))
+
+	AddCmd.SetArgs([]string{"../../../mock/bucketpolicy.json"})
+	err := AddCmd.Execute()
+	assert.NotNil(t, err)
+
+	rootOpts.SetZeroValues()
+	bucketPolicyOpts.SetZeroValues()
+}
 
 /*func TestExecuteSuccessEnabled2(t *testing.T) {
 	rootOpts := options.GetRootOptions()
--- cmd/bucketpolicy/remove/remove_test.go
+++ cmd/bucketpolicy/remove/remove_test.go
@@ -2,7 +2,10 @@ package remove
 
 import (
 	"context"
+	"errors"
 	"testing"
+
+	"github.com/aws/aws-sdk-go/service/s3/s3iface"
 
 	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/bilalcaliskan/s3-manager/cmd/root/options"
@@ -10,25 +13,23 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-/*var (
-	defaultGetBucketPolicyOutput = &s3.GetBucketPolicyOutput{
-		Policy: aws.String("{}"),
-	}
-	defaultGetBucketPolicyErr error
-)*/
+var (
+	defaultDeleteBucketPolicyOutput = &s3.DeleteBucketPolicyOutput{}
+	defaultDeleteBucketPolicyErr    error
+)
 
 func createSvc(rootOpts *options.RootOptions) (*s3.S3, error) {
 	return internalaws.CreateAwsService(rootOpts)
 }
 
 // Define a mock struct to be used in your unit tests
-/*type mockS3Client struct {
+type mockS3Client struct {
 	s3iface.S3API
-}*/
+}
 
-/*func (m *mockS3Client) GetBucketPolicy(input *s3.GetBucketPolicyInput) (*s3.GetBucketPolicyOutput, error) {
-	return defaultGetBucketPolicyOutput, defaultGetBucketPolicyErr
-}*/
+func (m *mockS3Client) DeleteBucketPolicy(input *s3.DeleteBucketPolicyInput) (*s3.DeleteBucketPolicyOutput, error) {
+	return defaultDeleteBucketPolicyOutput, defaultDeleteBucketPolicyErr
+}
 
 func TestExecuteTooManyArguments(t *testing.T) {
 	rootOpts := options.GetRootOptions()
@@ -56,7 +57,7 @@ func TestExecuteTooManyArguments(t *testing.T) {
 	bucketPolicyOpts.SetZeroValues()
 }
 
-/*func TestExecuteNoArgument(t *testing.T) {
+func TestExecuteSuccess(t *testing.T) {
 	rootOpts := options.GetRootOptions()
 	rootOpts.AccessKey = "thisisaccesskey"
 	rootOpts.SecretKey = "thisissecretkey"
@@ -65,21 +66,48 @@ func TestExecuteTooManyArguments(t *testing.T) {
 
 	ctx := context.Background()
 	RemoveCmd.SetContext(ctx)
-	svc, err := createSvc(rootOpts)
-	assert.NotNil(t, svc)
-	assert.Nil(t, err)
+
+	mockSvc := &mockS3Client{}
+	svc = mockSvc
+
+	defaultDeleteBucketPolicyErr = nil
 
 	RemoveCmd.SetContext(context.WithValue(RemoveCmd.Context(), options.S3SvcKey{}, svc))
 	RemoveCmd.SetContext(context.WithValue(RemoveCmd.Context(), options.OptsKey{}, rootOpts))
 
 	RemoveCmd.SetArgs([]string{})
-	err = RemoveCmd.Execute()
+	err := RemoveCmd.Execute()
+	assert.Nil(t, err)
+
+	rootOpts.SetZeroValues()
+	bucketPolicyOpts.SetZeroValues()
+}
+
+func TestExecuteFailure(t *testing.T) {
+	rootOpts := options.GetRootOptions()
+	rootOpts.AccessKey = "thisisaccesskey"
+	rootOpts.SecretKey = "thisissecretkey"
+	rootOpts.Region = "thisisregion"
+	rootOpts.BucketName = "thisisbucketname"
+
+	ctx := context.Background()
+	RemoveCmd.SetContext(ctx)
+
+	mockSvc := &mockS3Client{}
+	svc = mockSvc
+
+	defaultDeleteBucketPolicyErr = errors.New("dummy error")
+
+	RemoveCmd.SetContext(context.WithValue(RemoveCmd.Context(), options.S3SvcKey{}, svc))
+	RemoveCmd.SetContext(context.WithValue(RemoveCmd.Context(), options.OptsKey{}, rootOpts))
+
+	RemoveCmd.SetArgs([]string{})
+	err := RemoveCmd.Execute()
 	assert.NotNil(t, err)
 
 	rootOpts.SetZeroValues()
 	bucketPolicyOpts.SetZeroValues()
 }
-*/
 
 /*func TestExecuteSuccessEnabled(t *testing.T) {
 	rootOpts := options.GetRootOptions()
