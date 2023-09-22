3fbbcc9c2f04137a684d2b5a29138c9f02fe940f
bilalcaliskan <bilalcaliskan@protonmail.com>
2023-09-22 18:33:58 +0000

    docs: edit

--- cmd/clean/clean.go
+++ cmd/clean/clean.go
@@ -16,6 +16,7 @@ import (
 	"github.com/spf13/cobra"
 )
 
+
 func init() {
 	cleanOpts = options.GetCleanOptions()
 	cleanOpts.InitFlags(CleanCmd)
--- hack/coverage.sh
+++ hack/coverage.sh
@@ -2,4 +2,4 @@
 
 go test -tags "unit e2e integration" ./... -race -p 1 -coverprofile=coverage.txt -covermode=atomic
 go tool cover -html=coverage.txt -o cover.html
-#open cover.html
+open cover.html
